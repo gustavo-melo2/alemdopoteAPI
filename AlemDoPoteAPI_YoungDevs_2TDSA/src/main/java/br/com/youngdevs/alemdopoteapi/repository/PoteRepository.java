fc5471122083eea79c85b72857ac279b54e511af
Gustavo Melo <gtvmelo10@gmail.com>
2021-10-22 20:42:29 -0300

    Delete AlemDoPoteAPI_YoungDevs_2TDSA directory

--- AlemDoPoteAPI_YoungDevs_2TDSA/src/main/java/br/com/youngdevs/alemdopoteapi/repository/PoteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.youngdevs.alemdopoteapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.youngdevs.alemdopoteapi.models.Pote;
-
-public interface PoteRepository extends JpaRepository<Pote, Long>{
-
-}
